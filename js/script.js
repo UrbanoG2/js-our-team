1ece22413c27c074bfc6e5652f1892bda598b998
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-12-09 15:52:32 +0100

    first part done

--- js/script.js
+++ js/script.js
@@ -30,3 +30,50 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+console.log(team[0]);
+
+
+//recupero il mio container dal dom
+
+const container = document.querySelector(".team-container");
+
+//richiamo la funzione creata sotto
+
+createCard(team, container);
+
+
+// una funzione per creare qualcosa nel dom, che va attaccato al container preso in precedenza
+
+function createCard (array, container) {
+
+  container.innerHTML = " ";
+
+  for (let i = 0; i < array.length; i++) {
+
+    const object = array[i];
+
+    //scrivo a mano l'HTML (odio i backtick)
+
+    const containedDiv = `
+      <div class="team-card">
+        
+        <div class="card-image">
+          <img src="img/${object.image}" alt="">
+        </div>
+        
+
+        <div class="card-text">
+          <h3>${object.name}</h3>
+          <p>${object.role}</p>
+        </div>
+
+      </div>
+      
+      `;
+
+    //stampo in pagina ogni volta conservando ciò che ho stampato prima
+
+    container.innerHTML += containedDiv;
+  }
+}
